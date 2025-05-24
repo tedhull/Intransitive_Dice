04a97320de73a29f24238c411af3d5ef0d8f4d49
tedhull <jedi.f.sh@gmail.com>
2025-05-24 18:11:33 +0400

    help message update

--- src/Messenger.php
+++ src/Messenger.php
@@ -2,7 +2,7 @@
 namespace App;
 class Messenger
 {
-    const string HELP_MESSAGE = "Here's how you play the game!";
+    const string HELP_MESSAGE = "Here's how you play the game!\nFirst of all add the set of 3 or more dice that respects the rules of the \033[1mGeneralized Intransitive Dice Game\033[0m\nthat means you need the set of dice where each die will be stronger than previous one, and the first die should be stronger than last.";
     public static function message($text): void
     {
         echo "\n" . $text;
